3ba50e4c7405221d191fb6fd55204428a4aa11d4
sunmini11 <yaifirst_sunmini@hotmail.com>
2016-11-03 12:07:27 +0700

    json parser

--- app/src/main/java/com/egco428/xmlparser/MainActivity.java
+++ app/src/main/java/com/egco428/xmlparser/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
     private String url1 = "http://api.openweathermap.org/data/2.5/weather?q=";
-    private String url2 = "&mode=xml&appid=b994c35873c281b5257ad8edbf3c6dfb";
+    private String url2 = "&appid=a21221acd1aef50e41c78a401bba2413";
     private EditText location,country,temperature,humidity,pressure;
-    private HandleXML obj;
+    private JsonParser obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         location = (EditText)findViewById(R.id.locInput);
@@ -20,21 +23,27 @@ public class MainActivity extends AppCompatActivity {
         temperature = (EditText)findViewById(R.id.tempTxt);
         humidity = (EditText)findViewById(R.id.humidTxt);
         pressure = (EditText)findViewById(R.id.pressTxt);
+
+
     }
 
     public void openWeatherMethod(View view){
         String url = location.getText().toString();
         String finalUrl = url1 + url + url2;
 
-        obj = new HandleXML(finalUrl);
-        obj.fetchXML();
+        obj = new JsonParser(finalUrl);
+        obj.fetchJSON();
         while (obj.parsingComplete);
         country.setText(obj.getCountry());
-        temperature.setText(obj.getTemperature());
+
         humidity.setText(obj.getHumidity());
         pressure.setText(obj.getPressure());
 
 
+        float kelvin = Float.parseFloat(obj.getTemperature());
+        float cel = kelvin-273;
+        DecimalFormat format = new DecimalFormat("0.00");
+        temperature.setText(String.valueOf(format.format(cel)));
     }
 
 }
